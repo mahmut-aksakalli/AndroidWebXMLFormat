e03f11322ae0953e788778e540f9f5901cd3c0d0
mahmut <mahmutaksakalli01@gmail.com>
2018-05-03 17:46:03 +0300

    Initial commit Dependecies added --> picasso, butterknife, retrofit2, simplexml-converter, recyclerview

---
+++ app/src/main/java/hr/ferit/mahmutaksakalli/androidwebxmlformat/view/MainActivity.java
@@ -0,0 +1,15 @@
+package hr.ferit.mahmutaksakalli.androidwebxmlformat.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hr.ferit.mahmutaksakalli.androidwebxmlformat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
